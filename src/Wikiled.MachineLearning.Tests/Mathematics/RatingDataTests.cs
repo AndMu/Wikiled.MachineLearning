6b243e9ad67dbbb9dca3ec6143d6e951d4a3b112
Andrius Mudinas <andrius@wikiled.com>
2018-07-16 09:31:55 +0100

    Moving rating calculator

---
+++ src/Wikiled.MachineLearning.Tests/Mathematics/RatingDataTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using Wikiled.MachineLearning.Mathematics;
+
+namespace Wikiled.MachineLearning.Tests.Mathematics
+{
+    [TestFixture]
+    public class RatingDataTests
+    {
+        [Test]
+        public void IsStrongPositive()
+        {
+            RatingData data = new RatingData();
+            Assert.IsFalse(data.IsStrong);
+            data.Positive += 2;
+            Assert.IsFalse(data.IsStrong);
+            data.Positive += 2;
+            Assert.IsTrue(data.IsStrong);
+
+            data.Negative += 2;
+            Assert.IsFalse(data.IsStrong);
+            data.Positive += 2;
+            Assert.IsTrue(data.IsStrong);
+        }
+
+        [Test]
+        public void IsStrongNegative()
+        {
+            RatingData data = new RatingData();
+            Assert.IsFalse(data.IsStrong);
+            data.Negative += 2;
+            Assert.IsFalse(data.IsStrong);
+            data.Negative += 2;
+            Assert.IsTrue(data.IsStrong);
+
+            data.Positive += 2;
+            Assert.IsFalse(data.IsStrong);
+            data.Negative += 2;
+            Assert.IsTrue(data.IsStrong);
+        }
+    }
+}
---
+++ src/Wikiled.MachineLearning/Mathematics/RatingData.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Wikiled.MachineLearning.Mathematics
+{
+    public class RatingData : ICloneable
+    {
+        public bool HasValue => Positive > 0 || Negative > 0;
+
+        public bool IsPositive => RawRating > 0;
+
+        public bool IsStrong
+        {
+            get
+            {
+                var difference = Positive - Negative;
+                if (Math.Abs(difference) < 4)
+                {
+                    return false;
+                }
+
+                if (Positive == 0 ||
+                    Negative == 0)
+                {
+                    return true;
+                }
+
+                var ratio = Positive > Negative ? Positive / Negative : Negative / Positive;
+                return Math.Abs(ratio) >= 2;
+            }
+        }
+
+        public double Negative { get; set; }
+
+        public double Positive { get; set; }
+
+        public double? RawRating
+        {
+            get => HasValue ? RatingCalculator.Calculate(Positive, Negative) : (double?)null;
+            set
+            {
+            }
+        }
+
+        public double? StarsRating
+        {
+            get => RatingCalculator.CalculateStarsRating(RawRating);
+            set
+            {
+            }
+        }
+
+        public static RatingData Accumulate(IEnumerable<RatingData> items)
+        {
+            var data = new RatingData();
+            foreach (var ratingData in items)
+            {
+                data.Positive += ratingData.Positive;
+                data.Negative += ratingData.Negative;
+            }
+
+            return data;
+        }
+
+        public object Clone()
+        {
+            return new RatingData
+            {
+                Negative = Negative,
+                Positive = Positive
+            };
+        }
+    }
+}
