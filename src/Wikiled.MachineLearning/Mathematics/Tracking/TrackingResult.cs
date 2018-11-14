a7cc8fa6e9ed5bfd86f634f238673d9ac3ae6a3d
Andrius Mudinas <andrius@wikiled.com>
2018-11-14 14:56:32 +0000

    Adding results

---
+++ src/Wikiled.MachineLearning/Mathematics/Tracking/TrackingResult.cs
@@ -0,0 +1,14 @@
+﻿namespace Wikiled.MachineLearning.Mathematics.Tracking
+{
+    public class TrackingResult
+    {
+        public double? Average { get; set; }
+
+        public int TotalMessages { get; set; }
+
+        public override string ToString()
+        {
+            return $"Average Result: {Average}({TotalMessages})";
+        }
+    }
+}
